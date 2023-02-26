930a848558e00eb9d8aa53a5c10db0c0d58ab6c5
Ry <OfficiallyRy@outlook.com>
2023-02-25 17:29:17 -0700

    Added missing inputs in the Account Edit form

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,21 +1,26 @@
 <template>
-  <div id="coverImg" class="about text-center pb-1">
+  <div id="coverImg" class="about text-center pb-2">
     <h1 class="my-shadow">Welcome, {{ account.name }}.</h1>
     <img class="rounded-circle" :src="account.picture" alt="" height="200" width="200"/>
-    <p class="mt-3 my-shadow">{{ account.email }}</p>
   </div>
-  <div class="d-flex justify-content-center mt-2">
+  <div class="d-flex justify-content-center">
     <form @submit.prevent="editAccount()">
-      <label for="name">Name</label>
-      <input required v-model="editable.name" class="form-control mt-1 mb-3" type="text" placeholder="Name..." id="name">
+      <label for="name" class="mt-2">Name</label>
+      <input required v-model="editable.name" class="form-control  mb-2" type="text" placeholder="Name..." id="name">
       <label for="picture">Profile Picture</label>
-      <input v-model="editable.picture" type="url" class="form-control mt-1 mb-3" placeholder="Img URL..." id="picture">
+      <input v-model="editable.picture" type="url" class="form-control  mb-2" placeholder="Img URL..." id="picture">
       <label for="github">GitHub Profile</label>
-      <input v-model="editable.github" type="url" class="form-control mt-1 mb-3" id="github" placeholder="GitHub URL...">
+      <input v-model="editable.github" type="url" class="form-control  mb-2" id="github" placeholder="GitHub URL...">
       <label for="linkedin">Linkedin Profile</label>
-      <input v-model="editable.linkedin" type="url" id="linkedin" class="form-control mt-1 mb-3" placeholder="Linkedin URL...">
+      <input v-model="editable.linkedin" type="url" id="linkedin" class="form-control  mb-2" placeholder="Linkedin URL...">
       <label for="coverImg">Cover Image</label>
-      <input v-model="editable.coverImg" type="url" id="coverImg" class="form-control mt-1 mb-3" placeholder="Img URL...">
+      <input v-model="editable.coverImg" type="url" id="coverImg" class="form-control  mb-2" placeholder="Img URL...">
+      <label for="resume">Resume Link</label>
+      <input v-model="editable.resume" type="url" class="form-control  mb-2" id="resume" placeholder="Link to Resume...">
+      <label for="class">Class</label>
+      <input v-model="editable.class" type="text" id="class" placeholder="Class Name/Year..." class="form-control mb-2">
+      <input v-model="editable.graduated" :checked="editable.graduated" type="checkbox" id="graduated" class="form-check-input mb-2 s-5 me-2 text-center">
+      <label for="graduated" class="">Graduated?</label>
       <button type="submit" class="btn btn-outline-light w-100 mt-1">Save Changes</button>
     </form>
   </div>
@@ -29,7 +34,7 @@ import Pop from "../utils/Pop.js"
 export default {
   setup() {
     const editable = ref({})
-    async function setCoverImg(){
+    function setCoverImg(){
       document.getElementById('coverImg').style.backgroundImage = `url('${AppState.account?.coverImg}')`
     }
 
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,7 +1,37 @@
 <template>
   <div v-if="profile" class="container-fluid">
-    <div class="row">
-
+    <div class="row px-5 py-2">
+      <div class="col-12 elevation-2 my-grey">
+        <div class="row">
+          <div class="col-12 g-0">
+            <img :src="profile.coverImg" alt="" class="coverImg">
+          </div>
+          <div class="col-12 d-flex justify-content-between secretsauce align-items-center">
+            <div>
+              <img id="pfp" :src="profile.picture" :alt="profile.name" class="rounded-circle" height="180" width="180">
+            </div>
+            <div>
+              <a v-if="profile.github" :href="profile.github">
+                <i class="mdi mdi-github fs-2 me-3 iconsauce"></i>
+              </a>
+              <a v-if="profile.linkedin" :href="profile.linkedin">
+                <i class="mdi mdi-linkedin fs-2 me-3 iconsauce"></i>
+              </a>
+              <a v-if="profile.resume" :href="profile.resume">
+                <i class="mdi mdi-note-text-outline me-2 fs-2 iconsauce"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-12 g-0 px-3">
+            <span class="fs-5">{{ profile.class }}</span>
+            <span v-if="profile.graduated" class=""> - <i class="">Graduated</i></span>
+          </div>
+          <div class="col-12 g-0 px-3">
+            <span class="fs-4"><b>{{ profile.name }}</b></span>
+          </div>
+          <div class="col-12 g-0 mb-3 px-3"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +49,7 @@ export default {
   setup(){
     const route = useRoute();
     const profileId = route.params.profileId
+    const profile = AppState.profile
     async function getProfileById(){
       try {
         await profilesService.getProfileById(profileId)
@@ -54,5 +85,27 @@ export default {
 
 
 <style lang="scss" scoped>
+.secretsauce{
+  transform: translateY(-7vh);
+}
 
+.iconsauce{
+  cursor: pointer;
+}
+
+.iconsauce:hover{
+  animation-name: rainbow;
+  animation-iteration-count: infinite;
+  animation-duration: 5s;
+}
+
+.coverImg{
+  width: 100%;
+  height: 30vh;
+  overflow-y: hidden;
+}
+
+#pfp{
+  border: 2px solid black;
+}
 </style>
